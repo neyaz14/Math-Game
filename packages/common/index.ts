c2f02f5345d3a152ff5f192dd7097c6a77d3e2ef
Neyaz Morshid <neyazmorshid@gmail.com>
2026-09-20 19:16:47 +0600

    auth working successfully

--- packages/common/index.ts
+++ packages/common/index.ts
@@ -3,7 +3,7 @@ import { z, ZodError } from "zod";
 export const registerSchema = z.object({
   email: z.string().email("Invalid email address"),
   password: z.string().min(6, "Password must be at least 6 characters"),
-  username: z.string().min(3, "Username must be at least 3 characters"),
+  
 });
 
 export const loginSchema = z.object({
@@ -16,6 +16,7 @@ export type LoginInput = z.infer<typeof loginSchema>;
 
 export const zodErrorMessage = ({ error }: { error: ZodError }): string => {
   if (!error || !error.issues || error.issues.length === 0) {
+    console.log("inside zod error", error);
     return "Validation failed";
   }
   return error.issues.map((issue) => issue.message).join(", ");
